46533271c19163e624069ac58055b84a8d9f2644
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-23 14:14:58 -0700

    move scripts to head of html; refactor jQuery to $

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
-
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("This is an image.");
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    // alert("I told you, THIS IS A HEADER!");
+  });
+  
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  
+  $("img").click(function() {
+    alert("This is an image.");
+  })
 })
